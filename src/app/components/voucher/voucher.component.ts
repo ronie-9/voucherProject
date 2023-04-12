dbbcc43a8f04d69df0f6a2a66c2b528a0afde2a7
Iván Gergely <ronie999@gmail.com>
2023-04-12 15:38:05 +0200

    styling

--- src/app/components/voucher/voucher.component.ts
+++ src/app/components/voucher/voucher.component.ts
@@ -12,18 +12,51 @@ import { UserState } from '../../store/reducers/user.reducer';
 })
 export class VoucherComponent implements OnInit {
 	voucher: any;
+	countdown: string = "";
 
-	constructor(private store: Store<{ voucher: VoucherState; user: UserState }>) {}
+	constructor(private store: Store<{ voucher: VoucherState }>) {}
 
 	ngOnInit(): void {
 		this.store.dispatch(loadVenues());
 
 		this.store.select('voucher').subscribe((voucherState) => {
 			this.voucher = voucherState;
+			if (this.voucher.voucherData && this.voucher.voucherData.expiresAt) {
+				this.startCountdown(this.voucher.voucherData.expiresAt);
+			}
 		});
 	}
 
 	purchaseVoucher(): void {
 		this.store.dispatch(purchaseVoucher());
+	}
+
+	startCountdown(expiresAt: string): void {
+		/* mocking Date in the future
+		const today = new Date();
+		const tomorrow = today.setDate(today.getDate() + 1);
+		const endTime = new Date(tomorrow).getTime();
+		*/
+		
+		const endTime = new Date(expiresAt).getTime();
+		const interval = setInterval(() => {
+			const currentTime = new Date().getTime();
+			const timeRemaining = endTime - currentTime;
+
+			if (timeRemaining > 0) {
+				this.countdown = this.formatTimeRemaining(timeRemaining);
+			} else {
+				this.countdown = "Expired";
+				clearInterval(interval);
+			}
+		}, 1000);
+	}
+
+	formatTimeRemaining(timeRemaining: number): string {
+		const hours = Math.floor(timeRemaining / (1000 * 60 * 60));
+		const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+		return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 	}
 }
